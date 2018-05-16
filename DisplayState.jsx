d932ddc442dfa01c3e36cbaa843188d5924dcbd1
Yarden Hochman <yyyarden@gmail.com>
2018-05-16 12:47:00 -0700

    Update DisplayState.jsx

--- DisplayState.jsx
+++ DisplayState.jsx
@@ -1,17 +1,15 @@
 class DisplayState extends Container {
-  constructor(props) {
-    super();
-    this.state = {
+  state = {
       Display: null,
       ...props
     };
-  }
   trigger = (Display = null) => {
     this.setState({ Display });
   };
 }
 //Now when we want to call a new instance, we will say
 const LoginModalDisplayState = new DisplayState();
+//or
 const MoreInfoModalDisplayState = new DisplayState();
 //We can use use the same container for more advanced use cases. For example:
 const StepperDisplayState = Display => new DisplayState({ Display });
