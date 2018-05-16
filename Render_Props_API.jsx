700e3fd3c5e353747a73b1cd3163567bc7bde9d8
Yarden Hochman <yyyarden@gmail.com>
2018-05-16 12:53:13 -0700

    Update Render_Props_API.jsx

--- Render_Props_API.jsx
+++ Render_Props_API.jsx
@@ -1,3 +1,11 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { Provider, Subscribe } from "unstated";
+import UI from "../UI";
+import { PROJECT_DETAILS } from "./Queries";
+import WikiState from "./Store";
+import WikiView from "./View";
+
 export default ({ match: { params: { id } } }) => (
   <Query query={PROJECT_DETAILS} variables={{ id }}>
     {res => (
